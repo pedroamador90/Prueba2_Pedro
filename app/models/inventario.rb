1530c95d933d6272a25721b98fec3dcddb4e2e56
pedro <pedro.amador1090@gmail.com>
2016-04-15 21:00:25 -0500

    Scopes

--- app/models/inventario.rb
+++ app/models/inventario.rb
@@ -1,3 +1,4 @@
 class Inventario < ActiveRecord::Base
 	has_one :operacion
+	#scope :primeroperador, -> Inventario.count 
 end
--- app/models/operacion.rb
+++ app/models/operacion.rb
@@ -1,4 +1,7 @@
 class Operacion < ActiveRecord::Base
   belongs_to :trabajador
   belongs_to :inventario
+
+  scope :ultimos5, -> {limit 5}
+  scope :operacionesultimodia, -> {where('created_at >= ?', Date.today-1.day)} #al día de ayer, mayor igual porque guardamos hora
 end
